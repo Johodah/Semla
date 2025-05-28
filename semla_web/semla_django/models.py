ebd8c2fe388716dbd4faf9cf3cf92b332e832441
Johohoa <johnny.dahle@hotmail.com>
2025-05-28 09:14:40 +0200

    installerat Pillow, migrerat data och bilder

--- semla_web/semla_django/models.py
+++ semla_web/semla_django/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
+class Semla(models.Model):
+    bakery = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    picture = models.ImageField(upload_to='produkter/')
+    vegan = models.BooleanField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    kind = models.CharField(max_length=50)
+
+    def _str_(self):
+        return f"{self.bakery} - {self.city} - {self.kind} - {self.price} SEK"
 # Create your models here.
